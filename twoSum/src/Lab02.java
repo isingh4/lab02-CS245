ef75243c312b360b4028a8a396e5543f20ef4698
Isha Singh <65196802+isingh4@users.noreply.github.com>
2021-02-16 15:59:22 -0800

    Update Lab02.java

--- twoSum/src/Lab02.java
+++ twoSum/src/Lab02.java
@@ -1,22 +1,9 @@
 import java.util.Arrays;
 
 public class Lab02 {
-    public void selectionSort(int num[]) {
-        for (int i = 0; i < num.length - 1; i++) {
-            int temp = i;
-            for (int j = i+1; j < num.length; j++) {
-                if (num[j] < num[temp]) {
-                    temp = j;
-                }
-            }
-            int x = num[temp];
-            num[temp] = num[i];
-            num[i] = x;
-        }
-    }
+
 
     public int[] twoSum(int[] num, int sum) {
-        selectionSort(num);
         int [] sumArray= new int [2];
         int [] error = {-1};
         for (int i = 0; i < num.length-1; i++) {
